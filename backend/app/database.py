25f2d134452ed4399f97db6df8e0758381ddda2d
Parascite_RAP <rajaryan0734@gmail.com>
2026-03-22 13:26:47 +0530

    database connected

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,16 +2,16 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
-
+ 
 load_dotenv()
-
+ 
 
 DATABASE_URL = os.getenv("DATABASE_URL") or "sqlite:///./sherlock.db"
-
+ 
 
 if DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
-
+ 
 
 if "sqlite" in DATABASE_URL:
     engine = create_engine(
@@ -26,18 +26,19 @@ else:
         echo=False,
         future=True
     )
-
+ 
 SessionLocal = sessionmaker(
     bind=engine,
     autoflush=False,
     autocommit=False,
 )
-
+ 
 Base = declarative_base()
-
+ 
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
+ 
